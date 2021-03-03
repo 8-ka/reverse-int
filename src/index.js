09c881c094fc37c5ad1359461f6575a93d9ab955
Anton Bersenev <breaka@bk.ru>
2021-03-03 22:22:23 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+    let result = '';
+    let numAbs = Math.abs(num);
+    let strNum = String(numAbs);
+    
+    for (let i = 1; i <= strNum.length; i++) {
+        result += strNum[strNum.length - i];
+    };
+    
+    return Number(result)
 }
